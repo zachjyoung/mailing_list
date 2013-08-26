785ca9ff6b3604b7e5140e6f2d3d3283c4b54ed5
Zach Young <zachjyoung@gmail.com>
2013-08-26 08:48:18 -0400

    initial commit, added assignment arrays

---
+++ mailing_list.rb
@@ -0,0 +1,31 @@
+salutations = [
+  'Mr.',
+  'Mrs.',
+  'Mr.',
+  'Dr.',
+  'Ms.'
+]
+
+first_names = [
+  'John',
+  'Jane',
+  'Sam',
+  'Louise',
+  'Kyle'
+]
+
+last_names = [
+  'Dillinger',
+  'Cook',
+  'Livingston',
+  'Levinger',
+  'Merlotte'
+]
+
+addresses = [
+  '33 Foolish Lane, Boston MA 02210',
+  '45 Cottage Way, Dartmouth, MA 02342',
+  "54 Sally's Court, Bridgewater, MA 02324",
+  '4534 Broadway, Boston, MA 02110',
+  '4231 Cynthia Drive, Raynham, MA 02767'
+]
